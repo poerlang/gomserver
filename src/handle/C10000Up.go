cca28175495351f9dd5fe6f798e64bf7e8f7dbbd
moketao <moketao@qq.com>
2014-02-02 00:54:07 +0800

    v2014年2月2日0:54:03

--- src/handle/C10000Up.go
+++ src/handle/C10000Up.go
@@ -2,14 +2,20 @@ package handle
 
 import (
 	. "base"
-	_ "fmt"
+	"fmt"
 )
 
 type C10000Up struct {
-	v1 int32
-	v2 int32
+	uname string //用户名
+	pwd   string //密码
+	ctime int32  //创建时间
 }
 
 func f10000Up(c uint16, p *Pack) interface{} {
-	return p.ReadBytesRemain()
+	d := new(C10000Up)
+	d.uname = p.ReadString()
+	d.pwd = p.ReadString()
+	d.ctime = p.ReadInt32()
+	fmt.Println(d) //需删除，否则影响性能
+	return nil     //需修改，返回 []byte 类型的数据，否则客户端无法收到返回数据
 }
--- src/handle/handle.go
+++ src/handle/handle.go
@@ -16,16 +16,14 @@ var CMD CmdStuct                            //以结构形式存在的协议
 
 type CmdStuct struct {
 	//moeditor struct start
-	C1       ACMD
-	C2       ACMD
+
 	C10000up ACMD
 	//moeditor struct end
 }
 
 func init() {
 	//moeditor init start
-	CMD.C1 = ACMD{1, f1}
-	CMD.C2 = ACMD{2, f2}
+
 	CMD.C10000up = ACMD{10000, f10000Up}
 	//moeditor init end
 
@@ -38,10 +36,4 @@ func init() {
 			DIC[t.Code] = t //将协议写到 map 中
 		}
 	}
-}
-func f1(c uint16, p *Pack) interface{} {
-	return []byte("协议处理函数f1")
-}
-func f2(c uint16, p *Pack) interface{} {
-	return []byte("协议处理函数f2")
 }
