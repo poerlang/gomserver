70b683a52aa8c2011105ac52064a5c01f05497e7
moketao <moketao@qq.com>
2014-03-09 00:59:38 +0800

    修正守护进程相关flag解析错误的问题

--- src/mserver/main.go
+++ src/mserver/main.go
@@ -9,7 +9,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"os/exec"
+	//"os/exec"
 	//"handle"
 	"io"
 	"log"
@@ -23,18 +23,21 @@ const (
 )
 
 func main() {
-	flag.Parse()
 	//守护进程，开始
 	d := flag.Bool("d", false, "Whether or not to launch in the background(like a daemon)")
+	flag.Parse()
+	fmt.Println(*d)
 	if *d {
 		fmt.Println(os.Args[0] + " will run in background.")
 		filePath, _ := filepath.Abs(os.Args[0]) //将命令行参数中执行文件路径转换成可用路径
-		cmd := exec.Command(filePath, os.Args[2:]...)
+		//cmd := exec.Command(filePath, os.Args[2:]...)
 		//将其他命令传入生成出的进程
-		cmd.Stdin = os.Stdin //给新进程设置文件描述符，可以重定向到文件中
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		cmd.Start() //开始执行新进程，不等待新进程退出
+		//cmd.Stdin = os.Stdin //给新进程设置文件描述符，可以重定向到文件中
+		//cmd.Stdout = os.Stdout
+		//cmd.Stderr = os.Stderr
+		//cmd.Start() //开始执行新进程，不等待新进程退出
+		args := append([]string{filePath}, os.Args[2:]...)
+		os.StartProcess(filePath, args, &os.ProcAttr{Files: []*os.File{os.Stdin, os.Stdout, os.Stderr}})
 		return
 	}
 	//守护进程，结束
